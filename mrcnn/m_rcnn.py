4a05c25a81d76b7d0376842b2a09b0391736b4a9
MittalNeha <neha.cool@gmail.com>
2022-10-11 18:59:31 +0530

    changes to train num_classes =2
    training was failing for more than one class
    TODO: Only changed in config. need to understand the model

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -43,8 +43,9 @@ class CustomConfig(Config):
     def __init__(self, num_classes):
 
         if num_classes > 1:
-            raise ValueError("{} classes were found. This is a DEMO version, and it only supports 1 class. Get the PRO version to"
-                  " continue the training.".format(num_classes))
+            # raise ValueError("{} classes were found. This is a DEMO version, and it only supports 1 class. Get the PRO version to"
+            #       " continue the training.".format(num_classes))
+            self.NUM_CLASSES += (num_classes -1)
 
         classes_number = num_classes
         super().__init__()
